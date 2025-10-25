b04777105875a4cffae6e4b4b89e50553bf013be
anandarosyid <rosyidananda28@gmail.com>
2025-10-25 23:17:23 +0700

    modivikasi 1

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -3,6 +3,7 @@ public class SiakadFor05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double nilai, tertinggi=0, terendah=100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i =1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,10 +15,18 @@ public class SiakadFor05 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+                
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
         
     }
 }
